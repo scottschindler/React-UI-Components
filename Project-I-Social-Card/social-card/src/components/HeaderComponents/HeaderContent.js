3c9274fa19c8b72847e74e2439e479221ce266e4
Scott Schindler <scottschindler29@gmail.com>
2019-06-30 22:26:08 -0400

    changes

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Header.css';
 
 const Content = () => {
-    return <p class="content1">Let's learn React by building simple intefaces with componenet. Don't try to overthink it, 
-    just keep it simple <br></br> and have fun.
-     Once you feel comfortable using componenet you are well on your way to mastering React</p>
+    return <p class="content1">Let's learn React by building simple intefaces with componenet. Don't try to <br></br> overthink it, 
+    just keep it simple and have fun.
+     Once you feel comfortable using <br></br>componenet you are well on your way to mastering React</p>
 }
 
 export default Content;
